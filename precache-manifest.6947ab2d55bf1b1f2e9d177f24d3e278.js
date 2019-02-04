ddddb5ff5d794a86481c269eee5a6e811c8d29c1
shummi7 <sharmila16moorthi@gmail.com>
2019-02-04 13:38:52 -0800

    Updates

---
+++ precache-manifest.6947ab2d55bf1b1f2e9d177f24d3e278.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/testrobo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "196e99dfee986b60a5e5",
+    "url": "/testrobo/static/js/runtime~main.196e99df.js"
+  },
+  {
+    "revision": "5047a91318f067d882fd",
+    "url": "/testrobo/static/js/main.5047a913.chunk.js"
+  },
+  {
+    "revision": "d1a33af02243ac0de1fb",
+    "url": "/testrobo/static/js/1.d1a33af0.chunk.js"
+  },
+  {
+    "revision": "5047a91318f067d882fd",
+    "url": "/testrobo/static/css/main.61fd09c0.chunk.css"
+  },
+  {
+    "revision": "d1a33af02243ac0de1fb",
+    "url": "/testrobo/static/css/1.bb5147f2.chunk.css"
+  },
+  {
+    "revision": "62a6e099aa3762e21b2d88c4942eb54f",
+    "url": "/testrobo/index.html"
+  }
+];
